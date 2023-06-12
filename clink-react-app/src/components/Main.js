815e04b298e1a2acd38d5a8c0d5e6818914f69e6
HyeyoungCho <HyeyoungCho97@gmail.com>
2023-06-12 12:27:32 +0900

    add main component

--- clink-react-app/src/components/Main.js
+++ clink-react-app/src/components/Main.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import '../styles/Main.css';
+import React from "react";
+import "../styles/Main.css";
+import Menu from "./Menu";
 
 const Main = () => {
   return (
     <div className="main">
       <div className="mainBackgroundTop"></div>
       <div className="mainBackgroundBottom">
-        <div className="mainMenu">{/* 메뉴 넣을 자리  */}</div>
+        <div className="mainMenu">
+          <Menu />
+        </div>
         <div className="mainContent"></div>
       </div>
     </div>
