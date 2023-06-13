18faaf659f316f888b1d139857d29c7bc1dd29fd
배동우 <ehddn5410@naver.com>
2023-06-13 10:59:07 +0900

    Add TableChart

--- clink-react-app/src/App.js
+++ clink-react-app/src/App.js
@@ -1,16 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import TableList from './components/TableChart';
+import './styles/css/TableChart.scss';
+const createTitle = () => {
+  const titleList = [];
+  for (let i = 0; i < 4; i++) {
+    titleList.push({
+      id: i,
+      title: `title${i}`,
+    });
+  }
+  return titleList;
+};
+const createContents = () => {
+  const contentList = [];
+  for (let i = 0; i < 5; i++) {
+    contentList.push({
+      id: i,
+      date: `date${i}`,
+      category: `category${i}`,
+      content: `content${i}`,
+      price: `price${i}`,
+    });
+    // console.log(contentList[i]);
+    // console.log(Object.values(contentList[i])[1]);
+  }
+  return contentList;
+};
 function App() {
-  return (
-    <table>
-      <tr>
-        <th>거래날짜</th>
-        <th>카테고리</th>
-        <th>내용</th>
-        <th>금액</th>
-      </tr>
-    </table>
-  );
+  const [titles, setTitles] = useState(createTitle);
+  const [contents, setContents] = useState(createContents);
+  return <TableList titles={titles} contents={contents} />;
 }
 
 export default App;
