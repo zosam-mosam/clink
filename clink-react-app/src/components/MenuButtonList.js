f19635f478f041eef3315f772800764ea3f63e3f
soieu <soieu9898@gmail.com>
2023-06-13 17:45:59 +0900

    Edit MenuButtonList add admin version

--- clink-react-app/src/components/MenuButtonList.js
+++ clink-react-app/src/components/MenuButtonList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/MenuButtonList.css";
 import MenuButton from "./MenuButton";
 import targetDisabled from "../assets/targetDisabled.jpg";
@@ -6,21 +6,34 @@ import homeDisabled from "../assets/homeDisabled.png";
 import targetActive from "../assets/targetActive.png";
 import homeActive from "../assets/homeActive.png";
 
+
 const MenuButtonList = () => {
+  const [isAdmin, setIsAdmin] = useState(true);
+  const [commons, setCommons] = useState(["내 목표1", "내 목표2", "내 목표 3"]);
+  const admins = ["사용자 관리", "명언 관리", "뱃지 관리", "관리자계정 관리"];
+  const commonORAdminHandler = () => {
+    //주소창이 /admin이면 admin으로
+  }
   return (
     <div className="menuButtonList">
-      <MenuButton disabledImgSrc={homeDisabled} activeImgSrc={homeActive}>
-        Home
-      </MenuButton>
-      <MenuButton disabledImgSrc={targetDisabled} activeImgSrc={targetActive}>
-        내 목표 1
-      </MenuButton>
-      <MenuButton disabledImgSrc={targetDisabled} activeImgSrc={targetActive}>
-        내 목표 2
-      </MenuButton>
-      <MenuButton disabledImgSrc={targetDisabled} activeImgSrc={targetActive}>
-        내 목표 3
-      </MenuButton>
+      {isAdmin === true ? (
+        admins.map((admin, i) => (
+          <MenuButton disabledImgSrc={targetDisabled} activeImgSrc={targetActive}>
+            {admin}
+          </MenuButton>
+        ))
+      ) : (
+        <>
+          <MenuButton disabledImgSrc={homeDisabled} activeImgSrc={homeActive}>
+            Home
+          </MenuButton>
+          {commons.map((common, i) => (
+            <MenuButton disabledImgSrc={targetDisabled} activeImgSrc={targetActive}>
+              {common}
+            </MenuButton>
+          ))}
+        </>
+      )}
     </div>
   );
 };
---
+++ clink-react-app/src/components/TESTadminMenu.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Menu.css";
+import MenuButtonList from "./MenuButtonList";
+
+const Menu = () => {
+  return (
+    <div className="menu">
+      <MenuButtonList />
+    </div>
+  );
+};
+
+export default Menu;
