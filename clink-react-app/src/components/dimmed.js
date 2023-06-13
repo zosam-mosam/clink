32a430660367e77a30a9284304f9b3c4bbbc59ea
soieu <soieu9898@gmail.com>
2023-06-13 16:49:23 +0900

    Add passwordCheck

--- clink-react-app/src/components/dimmed.js
+++ clink-react-app/src/components/dimmed.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react'
 import '../styles/Dimmed.scss'
 import QuoteManagement from './modal/QuoteManagement';
 import UserManagement from './modal/UserManagement';
+import PasswordCheck from './modal/PasswordCheck';
+/* 스크롤 안되고 흑백표시된 부분 누르면 모달창 꺼지게도 기능 넣기*/
 /* 스크롤 안되고 흑백표시된 부분 누르면 모달창 꺼지게도 기능 넣기*/
 export default function Dimmed({backgroundColor}) {
     const [isModalOpen, setIsModalOpen] = useState(true);
@@ -13,8 +15,9 @@ export default function Dimmed({backgroundColor}) {
     <>
     {isModalOpen === true ? 
     <div className='background' style={{backgroundColor}}>
-        <QuoteManagement modalHandler={modalHandler}></QuoteManagement>
+        {/* <QuoteManagement modalHandler={modalHandler}></QuoteManagement> */}
         {/* <UserManagement modalHandler={modalHandler}></UserManagement> */}
+        <PasswordCheck modalHandler={modalHandler}></PasswordCheck>
     </div> : 
     null}
 
