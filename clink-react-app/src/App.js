2799b7897b31bbfc2f66dd9e9ae4b5fabcc1711f
zeegeon <zeegeon@naver.com>
2023-06-14 09:37:06 +0900

    Fix view

--- clink-react-app/src/App.js
+++ clink-react-app/src/App.js
@@ -1,11 +1,14 @@
 import Index from "./components/Index";
 import Login from "./components/Login";
 import FindIdPwd from "./components/FindIdPwd";
+import { useState } from "react";
 
 function App() {
+  const [content, setContent] = useState(["ID 찾기", "내용", "name"]);
+
   return (
     <>
-      <FindIdPwd />
+      <FindIdPwd content={content} />
     </>
   );
 }
--- clink-react-app/src/components/FindIdPwd.js
+++ clink-react-app/src/components/FindIdPwd.js
@@ -5,13 +5,15 @@ const handleSubmit = (event) => {
   event.preventDefault();
 };
 
-const FindIdPwd = () => {
+const findIdBtn = () => {};
+
+const FindIdPwd = (props) => {
   return (
     <>
       <div className="findIdPwdWrap">
         <div className="findIdPwd">
           <div className="innerWrap">
-            <h3>ID 찾기</h3>
+            <h3>{props.content[0]}</h3>
             <br />
             <div className="findIdPwd_textBox">
               회원 가입 시 입력하신 이름 + 이메일로
@@ -31,15 +33,48 @@ const FindIdPwd = () => {
               </form>
             </div>
             <br />
-            <button className="btn-findId" type="submit">
+            <button className="btn-findId" type="submit" onClick={findIdBtn}>
               아이디 찾기
             </button>
             <a href="index.do" className="btn-gotoLogin">
-              LOGIN
+              LOGIN 페이지로
             </a>
           </div>
         </div>
         <div className="v-line"></div>
+        <div className="findIdPwd">
+          <div className="innerWrap">
+            <h3>임시 비밀번호 발급</h3>
+            <br />
+            <div className="findIdPwd_textBox">
+              가입하신 아이디 + 이메일 본인인증을 통해
+              <br />
+              이메일로 임시 비밀번호를 보내드립니다.
+              <br />
+              <br />
+              확인 후 로그인하셔서 반드시 비밀번호를 변경하시기 바랍니다.
+            </div>
+            <br />
+            <div className="findIdPwd_inputBox">
+              <form onSubmit={handleSubmit}>
+                <input placeholder="ID" className="findIdPwd_input" />
+              </form>
+            </div>
+            <div className="findIdPwd_inputBox">
+              <form onSubmit={handleSubmit}>
+                <input placeholder="E-MAIL" className="findIdPwd_input" />{" "}
+                <br></br>
+              </form>
+            </div>
+            <br />
+            <button className="btn-findId" type="submit">
+              임시 비밀번호 발급
+            </button>
+            <a href="index.do" className="btn-gotoLogin">
+              LOGIN 페이지로
+            </a>
+          </div>
+        </div>
       </div>
     </>
   );
